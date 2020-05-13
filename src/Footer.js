c2ec4b5e1f17167fef1aee9f6052ccca631a41d0
kanokenji <aleksandrtarsevich77@gmail.com>
2020-05-13 13:55:50 -0600

    React app - Homework 1. Add footer block on the page.

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+const footerMenu = [
+    "Block 1",
+    "Block 2",
+    "Block 3",
+    "Block 4",
+];
+
+class FooterMenu extends Component{
+    render() {
+        return(
+            <div className="footer">
+                {footerMenu.map((value, index)=>{
+                    return <span className="block" key={index}><FooterMenuBlocks block={value}/></span>
+                })}
+            </div>
+        );
+    }
+}
+
+class FooterMenuBlocks extends Component{
+    render() {
+        return this.props.block;
+    }
+}
 
 function Footer() {
     return (
-        <div className="footer">
-            <h1>Footer</h1>
-        </div>
+        <FooterMenu />
     );
 }
 
