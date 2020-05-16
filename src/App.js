ec4dcc3ddfbeb88c89c5501a484d32225e589df0
kanokenji <aleksandrtarsevich77@gmail.com>
2020-05-16 06:25:27 -0600

    React app - Homework 1. Add new files structure.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Footer from "./Footer";
-import Content from "./Content";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Content from "./components/Content";
 
 function App() {
     return (
