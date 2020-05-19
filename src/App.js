e5fd2c185c561f900d470c744ddec82a4533acb6
kanokenji <aleksandrtarsevich77@gmail.com>
2020-05-19 11:09:37 -0600

    Home work for Lesson 2 (Transfer props to components). Some changes in description

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Footer from "./components/Footer/Footer";
 function App() {
     const topMenu = ['Contact', 'About', 'Products', 'Home'];
     const footerMenu = [
-        ['Footer-left-1', 'Footer-center-2', 'Footer-right-3'],
-        ['Footer-left-2.1', 'Footer-center-2.2', 'Footer-right-2.3'],
+        ['Footer-1', 'Footer-2', 'Footer-3'],
+        ['Footer-2.1', 'Footer-2.2', 'Footer-2.3'],
         ['Contact', 'About', 'Products', 'Home'],
     ];
     const copyright = 'Copyright © ' + (new Date).getFullYear() + ' Alexander Tarasevich';
