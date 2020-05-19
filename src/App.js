932f237ea03950213a293615ed2106805533dc8a
kanokenji <aleksandrtarsevich77@gmail.com>
2020-05-19 11:51:21 -0600

    Home work for Lesson 2 (Transfer props to components). Some changes in description

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Content from "./components/Content/Content";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-    const topMenu = ['Contact', 'About', 'Products', 'Home'];
+    const topMenu = ['Contact', 'About', 'Home Works', 'Home'];
     const footerMenu = [
         ['Footer-1', 'Footer-2', 'Footer-3'],
         ['Footer-2.1', 'Footer-2.2', 'Footer-2.3'],
         ['Contact', 'About', 'Products', 'Home'],
     ];
-    const copyright = 'Copyright © ' + (new Date).getFullYear() + ' Alexander Tarasevich';
+    const copyright = 'Copyright © ' + (new Date()).getFullYear() + ' Alexander Tarasevich';
     return (
         <div>
             <Header topMenu={topMenu} />
--- src/components/Header/MenuItem.js
+++ src/components/Header/MenuItem.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 function MenuItem(props) {
-    const url = (props.item == "Home") ? "" : "/" + props.item.toLowerCase().trim().replace(" ", "-");
+    console.log(window.location);
+    const url = (props.item === "Home") ? "/" : "/" + props.item.toLowerCase().trim().replace(" ", "-");
     return (
         <a className='menu-item' href={url}>
             {props.item}
