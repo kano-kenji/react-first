f4f32ff217f381b08e9d407143f64677726a5b1b
kanokenji <aleksandrtarsevich77@gmail.com>
2020-05-22 11:32:07 -0600

    Lesson 3 (Callback functionality). Home work - 3

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,28 +1,30 @@
 import React, {Component} from 'react';
 import FooterBlocks from "./FooterBlocks";
 
-class Footer extends Component{
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return(
-            <div className="footer">
-                {this.props.footerMenu.map(el => <FooterBlocks key={el} item={el} attempt={this.props.attempt}/>)}
-                <p className="copyright">{this.props.copyright}</p>
-            </div>
-        )
-    }
+function Footer(props) {
+    return (
+        <div className="footer">
+            <h3>useState in work</h3>
+            {props.footerMenu.map(el => <FooterBlocks key={el} item={el} attempt={props.attempt}/>)}
+            <p className="copyright">{props.copyright}</p>
+        </div>
+    );
 }
 
-// function Footer(props) {
-//     return (
-//         <div className="footer">
-//             {props.footerMenu.map(el => <FooterBlocks key={el} item={el} attempt={props.attempt}/>)}
-//             <p className="copyright">{props.copyright}</p>
-//         </div>
-//     );
+// class Footer extends Component{
+//     constructor(props) {
+//         super(props);
+//     }
+//
+//     render() {
+//         return(
+//             <div className="footer">
+//                 <h3>useState in work</h3>
+//                 {this.props.footerMenu.map(el => <FooterBlocks key={el} item={el} attempt={this.props.attempt}/>)}
+//                 <p className="copyright">{this.props.copyright}</p>
+//             </div>
+//         )
+//     }
 // }
 
 export default Footer;
--- src/components/Footer/FooterBlocks.js
+++ src/components/Footer/FooterBlocks.js
@@ -1,33 +1,36 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 
-class FooterBlocks extends Component{
-    constructor(props) {
-        super(props);
-    }
+function FooterBlocks(props) {
 
-    render() {
-        return(
-            <div className="column">
-                {this.props.item.map((el, key) =>
-                    <p key={key} onClick={()=>this.props.attempt(el)}>
-                        {el}
-                    </p>
-                )}
-            </div>
-        )
-    }
+    const [item, setItem] = useState();
+
+    return (
+        <div className="column">
+            {props.item.map((el, key) =>
+                <p key={key} onClick={()=>setItem(props.attempt(el))}>
+                    {el}
+                </p>
+            )}
+        </div>
+    );
 }
 
-// function FooterBlocks(props) {
-//     return (
-//         <div className="column">
-//             {props.item.map((el, key) =>
-//                 <p key={key} onClick={()=>props.attempt(el)}>
-//                     {el}
-//                 </p>
-//             )}
-//         </div>
-//     );
+// class FooterBlocks extends Component{
+//     constructor(props) {
+//         super(props);
+//     }
+//
+//     render() {
+//         return(
+//             <div className="column">
+//                 {this.props.item.map((el, key) =>
+//                     <p key={key} onClick={()=>this.props.attempt(el)}>
+//                         {el}
+//                     </p>
+//                 )}
+//             </div>
+//         )
+//     }
 // }
 
 export default FooterBlocks;
